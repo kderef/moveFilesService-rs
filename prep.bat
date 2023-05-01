c55f3f696a3c1ebd2d2b68ddb28fb24ae8da18d1
kvoid <kianheitkamp@outlook.com>
2023-05-01 15:57:28 +0200

    update ignore

--- prep.bat
+++ prep.bat
@@ -1,2 +1,7 @@
+:: run this after running `cargo build --release`
+
 @echo off
+
+:: NOTE rcedit must be installed for this to work.
+
 rcedit "target\release\moveFiles-service.exe" --set-icon "C:\Users\kianh\OneDrive\Afbeeldingen\10125_icons\imageres_1025.ico" 
